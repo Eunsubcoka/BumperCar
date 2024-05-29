e6eec8a790c4a09dfb755c46569aa870e1123001
Eunsubcoka <coka0602@hs.ac.kr>
2024-05-29 09:50:29 +0900

    흠

--- src/main/java/kr/co/tastyroad/common/FormController.java
+++ src/main/java/kr/co/tastyroad/common/FormController.java
@@ -24,7 +24,7 @@ public class FormController extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    response.setContentType("text/html; charset-utf-8");
+    response.setContentType("text/html; charset=utf-8");
 		
 		String action = request.getPathInfo();
 		String nextPage = "";
