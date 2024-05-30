99869a0f1b958478733cf82bf820b9a70463aff5
Eunsubcoka <coka0602@hs.ac.kr>
2024-05-30 17:20:28 +0900

    제목 : 레스토랑 게시판

--- src/main/java/kr/co/tastyroad/restaurant/model/dao/RestaurantDao.java
+++ src/main/java/kr/co/tastyroad/restaurant/model/dao/RestaurantDao.java
@@ -1,10 +1,10 @@
 package kr.co.tastyroad.restaurant.model.dao;
 
 import java.sql.Connection;
-
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import kr.co.tastyroad.common.DatabaseConnection;
 import kr.co.tastyroad.restaurant.model.dto.RestaurantDto;
@@ -24,7 +24,8 @@ public class RestaurantDao {
 
 	public RestaurantDto getRestaurant(int No) {
 		RestaurantDto result = new RestaurantDto();
-		String query = "Select * from restaurant where restaurantNo =  ? ";
+		String query = "Select * from restaurant "
+				+ " where restaurantNo = ?";
 		
 		try {
 			System.out.println("여기는 겟레스 실행문입니다.");
@@ -52,10 +53,43 @@ public class RestaurantDao {
 			e.printStackTrace();
 		}
 		
-		
-		
-		
 		return result;
 		
 	}
+	
+	// 리뷰 리스트 로직
+    public ArrayList<RestaurantDto> getMenuList(int No) {
+    	ArrayList<RestaurantDto> result = new ArrayList<RestaurantDto>();
+    	System.out.println(No);
+    
+   
+    String query = "select r.restaurantNo,m.foodNo,m.foodName,m.price from restaurant r "
+                 + "JOIN menu m ON m.restaurantNo = r.RESTAURANTNO where r.restaurantNo = ?";
+    
+    try {
+		pstmt = con.prepareStatement(query);
+		pstmt.setInt(1, No);
+		
+		ResultSet rs = pstmt.executeQuery();
+		while (rs.next()) {
+			RestaurantDto menu = new RestaurantDto();
+			String foodName = rs.getString("foodName");
+			int foodNo = rs.getInt("foodNo");
+			int price = rs.getInt("price");
+			System.out.println(foodName);
+			menu.setRestaurantNo(No);
+			menu.setMenu(foodName);
+			menu.setPrice(price);
+			menu.setFoodNo(foodNo);
+			result.add(menu) ;
+		}
+        return result;
+        
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+    return result;
+    }
+	
+	
 }
