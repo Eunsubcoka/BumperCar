99265ff7b450a734b01f8543a124c2ee954fe2cf
ahssa96 <ahssa96@naver.com>
2024-05-30 15:23:02 +0900

    nav 바

--- src/main/java/kr/co/tastyroad/notice/model/dao/noticeDao.java
+++ src/main/java/kr/co/tastyroad/notice/model/dao/noticeDao.java
@@ -14,31 +14,28 @@ public class noticeDao {
 	private Connection con;
 	private DatabaseConnection dc;
 	private PreparedStatement pstmt;
-	
+
 	public noticeDao() {
 		dc = new DatabaseConnection();
 		con = dc.connDB();
 	}
-	
-	public ArrayList<noticeDto> getList(PageInfo pi, String category, String searchText){
-		
-		
+
+	public ArrayList<noticeDto> getList(PageInfo pi, String category, String searchText) {
+
 		ArrayList<noticeDto> result = new ArrayList<>();
-		String query = "SELECT * FROM notice nt "
-				+ "JOIN Tasty_member m ON m.user_no = nt.user_no "
-				+ "WHERE " + category + " LIKE '%'||?||'%'"
-				+ "ORDER BY nt.noticeDate DESC "
+		String query = "SELECT * FROM notice nt " + "JOIN Tasty_member m ON m.user_no = nt.user_no " + "WHERE "
+				+ category + " LIKE '%'||?||'%'" + "ORDER BY nt.noticeDate DESC "
 				+ "OFFSET ? ROWS FETCH FIRST ? ROWS ONLY";
-		
+
 		try {
 			pstmt = con.prepareStatement(query);
 			pstmt.setString(1, searchText);
 			pstmt.setInt(2, pi.getOffset());
 			pstmt.setInt(3, pi.getBoardLimit());
-			
+
 			ResultSet rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				int no = rs.getInt("noticeNo");
 				String title = rs.getString("noticeTitle");
 				String content = rs.getString("noticeContent");
@@ -46,7 +43,7 @@ public class noticeDao {
 				String date = rs.getString("noticeDate");
 				int memberNo = rs.getInt("user_no");
 				String memberName = rs.getString("user_name");
-				
+
 				noticeDto noticeDto = new noticeDto();
 				noticeDto.setNoticeNo(no);
 				noticeDto.setNoticeTitle(title);
@@ -55,78 +52,74 @@ public class noticeDao {
 				noticeDto.setNoticeDate(date);
 				noticeDto.setUserNo(memberNo);
 				noticeDto.setUserName(memberName);
-				
+
 				result.add(noticeDto);
 			}
-			
+
 			pstmt.close();
 			con.close();
-		}catch(SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return result;
 	}
-	
+
 	public int getListCount(String category, String searchText) {
-		String query = "SELECT count(*) AS cnt FROM notice nt "
-				+ "JOIN Tasty_member m ON nt.user_no = m.user_no "
+		String query = "SELECT count(*) AS cnt FROM notice nt " + "JOIN Tasty_member m ON nt.user_no = m.user_no "
 				+ "WHERE " + category + " LIKE '%'||?||'%'";
-		
+
 		try {
 			pstmt = con.prepareStatement(query);
 			pstmt.setString(1, searchText);
-			
+
 			ResultSet rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				int result = rs.getInt("cnt");
 				return result;
 			}
-		}catch(SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return 0;
 	}
-	
+
 	public int enroll(noticeDto noticeDto) {
 		String query = "INSERT INTO notice VALUES(notice_seq.nextval,?, ?, default, default, ?)";
 		int result = 0;
-		
-		
+
 		try {
 			pstmt = con.prepareStatement(query);
 			pstmt.setString(1, noticeDto.getNoticeTitle());
 			pstmt.setString(2, noticeDto.getNoticeContent());
 			pstmt.setInt(3, noticeDto.getUserNo());
-			
+
 			result = pstmt.executeUpdate();
-		}catch(SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
-		
-		
+
 		return result;
 	}
-	
+
 	public noticeDto getDetail(int noticeNo) {
 		String query = "SELECT * FROM notice n LEFT OUTER JOIN notice_upload nu ON n.NOTICENO  = nu.NOTICENO  WHERE n.noticeNo = ?";
 		try {
 			pstmt = con.prepareStatement(query);
 			pstmt.setInt(1, noticeNo);
-			
+
 			ResultSet rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				int ntNo = rs.getInt("noticeNo");
 				String ntTitle = rs.getString("noticeTitle");
 				String ntContent = rs.getString("noticeContent");
 				String ntDate = rs.getString("noticeDate");
 				int ntView = rs.getInt("noticeView");
 				int mNo = rs.getInt("user_no");
-				
+
 				noticeDto noticeDto = new noticeDto();
 				noticeDto.setNoticeNo(ntNo);
 				noticeDto.setNoticeTitle(ntTitle);
@@ -134,54 +127,52 @@ public class noticeDao {
 				noticeDto.setNoticeDate(ntDate);
 				noticeDto.setNoticeView(ntView);
 				noticeDto.setUserNo(mNo);
-				
+
 				return noticeDto;
-				
-				
+
 			}
-		}catch(SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
-	
+
 	public void getWriter(noticeDto noticeDto) {
 		String query = "SELECT user_name FROM Tasty_member WHERE user_no = ?";
-		
+
 		try {
 			pstmt = con.prepareStatement(query);
 			pstmt.setInt(1, noticeDto.getUserNo());
-			
+
 			ResultSet rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				String name = rs.getString("user_name");
 				noticeDto.setUserName(name);
 			}
-		}catch(SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void getUserType(noticeDto noticeDto) {
 		String query = "SELECT user_type FROM notice n JOIN Tasty_member tm ON n.user_no = tm.user_no WHERE n.user_no = ?";
-		
+
 		try {
 			pstmt = con.prepareStatement(query);
 			pstmt.setInt(1, noticeDto.getUserNo());
-			
+
 			ResultSet rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				String type = rs.getString("user_type");
 				noticeDto.setUserType(type);
 			}
-		}catch(SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	public int setView(int noticeNo) {
 		String query = "UPDATE notice SET noticeView = noticeView+1 WHERE noticeNo = ?";
 		int result = 0;
@@ -189,160 +180,191 @@ public class noticeDao {
 			pstmt = con.prepareStatement(query);
 			pstmt.setInt(1, noticeNo);
 			return pstmt.executeUpdate();
-			
-		}catch(SQLException e) {
+
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return result;
-		
+
 	}
 
 	public int setEdit(noticeDto noticeDto) {
-		String query = "UPDATE notice "
-				+ "SET noticeTitle = ?,"
-				+ " noticeContent = ?"
-				+ "WHERE noticeNo = ?";
-		
+		String query = "UPDATE notice " + "SET noticeTitle = ?," + " noticeContent = ?" + "WHERE noticeNo = ?";
+
 		try {
 			pstmt = con.prepareStatement(query);
-			
+
 			pstmt.setString(1, noticeDto.getNoticeTitle());
 			pstmt.setString(2, noticeDto.getNoticeContent());
 			pstmt.setInt(3, noticeDto.getNoticeNo());
-			
+
 			int result = pstmt.executeUpdate();
 			return result;
-		}catch(SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return 0;
 	}
-	
+
 	public int setDelete(int noticeNo) {
 		String query = "DELETE FROM notice WHERE noticeNo = ?";
-		
+
 		try {
 			pstmt = con.prepareStatement(query);
 			pstmt.setInt(1, noticeNo);
-			
-			int result =pstmt.executeUpdate();
-			
+
+			int result = pstmt.executeUpdate();
+
 			return result;
-		}catch(SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return 0;
 	}
-	
+
 	public noticeDto selectNo(noticeDto noticeDto) {
-		String query = "SELECT noticeNo FROM notice "
-				+ "WHERE noticeNo = (SELECT MAX(noticeNo) FROM notice "
+		String query = "SELECT noticeNo FROM notice " + "WHERE noticeNo = (SELECT MAX(noticeNo) FROM notice "
 				+ "WHERE user_no = ?)";
-		
+
 		try {
 			pstmt = con.prepareStatement(query);
 			pstmt.setInt(1, noticeDto.getUserNo());
 			ResultSet rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				int noticeNo = rs.getInt("noticeNo");
 				noticeDto.setNoticeNo(noticeNo);
 				return noticeDto;
 			}
-			
-		}catch(SQLException e) {
+
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
-	
+
 	public int fileUpload(noticeDto noticeDto) {
 		String query = "INSERT INTO notice_upload VALUES(notice_upload_seq.nextval, ?, ?, ?, ?)";
-		
+
 		try {
-			pstmt= con.prepareStatement(query);
-			pstmt.setString(1, noticeDto.getFilePath() );
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, noticeDto.getFilePath());
 			pstmt.setString(2, noticeDto.getFileName());
 			pstmt.setInt(3, noticeDto.getUserNo());
 			pstmt.setInt(4, noticeDto.getNoticeNo());
-			
+
 			int result = pstmt.executeUpdate();
 			return result;
-			
-		}catch(SQLException e) {
+
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return 0;
 	}
-	
+
 	public void getFileName(noticeDto result) {
 		String query = "SELECT nu.nuNo, nu.nuName FROM notice n  LEFT OUTER JOIN notice_upload nu ON n.noticeNo = nu.noticeNo WHERE n.noticeNo = ?";
-		
+
 		try {
 			pstmt = con.prepareStatement(query);
 			pstmt.setInt(1, result.getNoticeNo());
 			ResultSet rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				int no = rs.getInt("nuNo");
 				String name = rs.getString("nuName");
-				
+
 				result.setFileNo(no);
 				result.setFileName(name);
-				
-				
+
 			}
-		}catch(SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public int setFileDelete(int fileNo) {
 		String query = "DELETE FROM notice_upload WHERE nuNo = ?";
-		
+
 		try {
 			pstmt = con.prepareStatement(query);
 			pstmt.setInt(1, fileNo);
 			int result = pstmt.executeUpdate();
 			return result;
-		}catch(SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return 0;
 	}
-	
+
 	public boolean deleteNotice(int noticeNo) {
-        boolean deleted = false;
-        
-        
-        try {
-        	//업로드된 이미지 먼저 삭제
-        	String query1 = "DELETE FROM notice_upload WHERE noticeNo = ?";
-            pstmt = con.prepareStatement(query1);
-            pstmt.setInt(1, noticeNo);
-            pstmt.executeUpdate();
-            pstmt.close();
-            
-            //해당 번호의 notice_upload를 삭제했으니 이제 본문 삭제 
-            String query2 = "DELETE FROM notice WHERE noticeNo = ?";
-            pstmt = con.prepareStatement(query2);
-            pstmt.setInt(1, noticeNo);
-            
-            int result = pstmt.executeUpdate();
-            
-            if (result > 0) {
-                deleted = true;
-            }
-            
-            pstmt.close();
-            con.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        
-        return deleted;
-    }
+		boolean deleted = false;
+
+		try {
+			// 업로드된 이미지 먼저 삭제
+			String query1 = "DELETE FROM notice_upload WHERE noticeNo = ?";
+			pstmt = con.prepareStatement(query1);
+			pstmt.setInt(1, noticeNo);
+			pstmt.executeUpdate();
+			pstmt.close();
+
+			// 해당 번호의 notice_upload를 삭제했으니 이제 본문 삭제
+			String query2 = "DELETE FROM notice WHERE noticeNo = ?";
+			pstmt = con.prepareStatement(query2);
+			pstmt.setInt(1, noticeNo);
+
+			int result = pstmt.executeUpdate();
+
+			if (result > 0) {
+				deleted = true;
+			}
+
+			pstmt.close();
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return deleted;
+	}
+	
+	
+	public noticeDto getLatestNotice() {
+	    String query = "SELECT * FROM notice nt JOIN Tasty_member m ON m.user_no = nt.user_no ORDER BY nt.noticeDate DESC FETCH FIRST 1 ROWS ONLY";
+
+	    try {
+	        pstmt = con.prepareStatement(query);
+	        ResultSet rs = pstmt.executeQuery();
+
+	        if (rs.next()) {
+	            int no = rs.getInt("noticeNo");
+	            String title = rs.getString("noticeTitle");
+	            String content = rs.getString("noticeContent");
+	            int views = rs.getInt("noticeView");
+	            String date = rs.getString("noticeDate");
+	            int memberNo = rs.getInt("user_no");
+	            String memberName = rs.getString("user_name");
+
+	            noticeDto latestNotice = new noticeDto();
+	            latestNotice.setNoticeNo(no);
+	            latestNotice.setNoticeTitle(title);
+	            latestNotice.setNoticeContent(content);
+	            latestNotice.setNoticeView(views);
+	            latestNotice.setNoticeDate(date);
+	            latestNotice.setUserNo(memberNo);
+	            latestNotice.setUserName(memberName);
+
+	            return latestNotice;
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+
+	    return null;
+	}
+
+
 }
--- src/main/java/kr/co/tastyroad/notice/model/service/noticeServiceImpl.java
+++ src/main/java/kr/co/tastyroad/notice/model/service/noticeServiceImpl.java
@@ -3,73 +3,73 @@ package kr.co.tastyroad.notice.model.service;
 import kr.co.tastyroad.notice.model.dao.noticeDao;
 import kr.co.tastyroad.notice.model.dto.noticeDto;
 
+import java.sql.Connection;
 import java.util.ArrayList;
 
 import kr.co.tastyroad.common.PageInfo;
 
-public class noticeServiceImpl implements noticeService{
-	
+public class noticeServiceImpl implements noticeService {
+
 	noticeDao noticeDao;
-	
+
 	public noticeServiceImpl() {
 		noticeDao = new noticeDao();
 	}
-	
+
 	@Override
-	public ArrayList<noticeDto> getList(PageInfo pi, String category, String searchText){
+	public ArrayList<noticeDto> getList(PageInfo pi, String category, String searchText) {
 		return noticeDao.getList(pi, category, searchText);
 	}
-	
+
 	@Override
 	public int getListCount(String category, String searchText) {
 		return noticeDao.getListCount(category, searchText);
 	}
-	
+
 	@Override
 	public int enroll(noticeDto noticeDto) {
 		return noticeDao.enroll(noticeDto);
 	}
-	
+
 	@Override
 	public noticeDto getDetail(int noticeNo) {
 		noticeDto result = noticeDao.getDetail(noticeNo);
-		
+
 		noticeDao.getWriter(result);
 		noticeDao.getUserType(result);
-		
+
 		int resultView = noticeDao.setView(result.getNoticeNo());
-		
-		if(resultView ==1) {
+
+		if (resultView == 1) {
 			return result;
 		}
-		
+
 		return null;
 	}
-	
+
 	@Override
 	public noticeDto getEditForm(int noticeNo) {
 		noticeDto result = noticeDao.getDetail(noticeNo);
 		noticeDao.getWriter(result);
-		
+
 		return result;
 	}
-	
-	
+
 	@Override
 	public int setEdit(noticeDto noticeDto) {
 		return noticeDao.setEdit(noticeDto);
 	}
-	
+
 	@Override
 	public int setDelete(int noticeNo) {
 		return noticeDao.setDelete(noticeNo);
 	}
-	
+
 	@Override
 	public noticeDto selectNo(noticeDto noticeDto) {
 		return noticeDao.selectNo(noticeDto);
 	}
-	
+
 
 	@Override
 	public int fileUpload(noticeDto noticeDto) {
@@ -88,7 +88,12 @@ public class noticeServiceImpl implements noticeService{
 
 	@Override
 	public boolean deleteNotice(int noticeNo) {
-	    return noticeDao.deleteNotice(noticeNo);
+		return noticeDao.deleteNotice(noticeNo);
 	}
 	
+	@Override
+	public noticeDto getLatestNotice() {
+	    return noticeDao.getLatestNotice();
+	}
+
 }
