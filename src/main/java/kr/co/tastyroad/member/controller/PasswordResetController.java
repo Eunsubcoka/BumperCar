b0edb631405544094eedad9c5627d96ddee7dfa5
baek <peterbaek93@naver.com>
2024-06-07 17:05:53 +0900

    이메일 비밀번호 재설정 완료.

--- src/main/java/kr/co/tastyroad/member/controller/PasswordResetController.java
+++ src/main/java/kr/co/tastyroad/member/controller/PasswordResetController.java
@@ -16,10 +16,6 @@ public class PasswordResetController extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private MemberServiceImpl memberService = new MemberServiceImpl();
 
-    public PasswordResetController() {
-        super();
-    }
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String token = request.getParameter("token");
         boolean isValid = memberService.verifyResetToken(token);
@@ -33,12 +29,11 @@ public class PasswordResetController extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	String token = request.getParameter("token");
+        String token = request.getParameter("token");
         String newPassword = request.getParameter("newPassword");
-        System.out.println("Token: " + token);
-        System.out.println("New Password: " + newPassword);
+
         boolean isReset = memberService.resetPassword(token, newPassword);
-        System.out.println("Password reset successful: " + isReset);
+
         request.setAttribute("isReset", isReset);
         RequestDispatcher dispatcher = request.getRequestDispatcher("/views/member/passwordResetResult.jsp");
         dispatcher.forward(request, response);
--- src/main/webapp/views/member/passwordResetResult.jsp
+++ src/main/webapp/views/member/passwordResetResult.jsp
@@ -1,5 +1,7 @@
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%
+    String accountId = request.getParameter("accountID");
+%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,19 +12,18 @@
 </head>
 <body>
     <%@ include file="/views/common/header.jsp"%>
-    <div class="form-container">
+    <div class="result-container">
         <h2>비밀번호 재설정 결과</h2>
         <c:choose>
             <c:when test="${isReset}">
-                <p>비밀번호가 성공적으로 재설정되었습니다.</p>
+                <p>비밀번호가 성공적으로 재설정되었습니다. 이제 로그인할 수 있습니다.</p>
             </c:when>
             <c:otherwise>
-                <p>비밀번호 재설정에 실패했습니다. 다시 시도해 주세요.</p>
+                <p>비밀번호 재설정에 실패했습니다. 다시 시도하세요.</p>
             </c:otherwise>
         </c:choose>
-        <a href="${pageContext.request.contextPath}/tastyForm/login.do">로그인 페이지로 이동</a>
+        <a href="${pageContext.request.contextPath}/views/member/login.jsp">로그인 페이지로 이동</a>
     </div>
     <%@ include file="/views/common/footer.jsp"%>
 </body>
 </html>
- 
